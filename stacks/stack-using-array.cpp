59e664601575a84e56ac60f0cc336a06c550c807
irkalra <irkalra@akamai.com>
2023-01-02 23:25:59 +0530

    Added stack implmentation using array

---
+++ stacks/stack-using-array.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+class Stack {
+  private:
+    int top;
+    int capacity;
+    int* arr;
+ 
+  public:
+    // default constructor
+    Stack() {
+      top = -1;
+      capacity = 1000;
+      arr = new int[capacity];
+    }
+
+    // parameterized constructor
+    Stack(int capacity) {
+      top = -1;
+      this->capacity = capacity;
+      arr = new int[capacity];
+    }
+
+    bool push(int x);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+ 
+bool Stack::push(int x) {
+  // edge case
+  if (top >= (capacity - 1)) {
+    cout << "Stack Overflow!";
+    return false;
+  }
+
+  arr[++top] = x;
+  cout << x << " pushed into stack\n";
+  return true;
+}
+ 
+int Stack::pop() {
+  if (top < 0) {
+    cout << "Stack Underflow!";
+    return 0;
+  }
+  
+  int x = arr[top--];
+  return x;
+}
+
+int Stack::peek() {
+  if (top < 0) {
+    cout << "Stack is Empty";
+    return 0;
+  }
+
+  int x = arr[top];
+  return x;
+}
+ 
+bool Stack::isEmpty() {
+  return (top < 0);
+}
+
+int main() {
+  class Stack s;
+  s.push(10);
+  s.push(20);
+  s.push(30);
+  cout << s.pop() << " Popped from stack\n";
+   
+  //print top element of stack after popping
+  cout << "Top element is : " << s.peek() << endl;
+  
+  //print all elements in stack :
+  cout <<"Elements present in stack : ";
+  while(!s.isEmpty()) {
+    // print top element in stack
+    cout << s.peek() <<" ";
+    // remove top element in stack
+    s.pop();
+  }
+  
+  return 0;
+}
