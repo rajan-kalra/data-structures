be668b559fb1c649142ef7c12d7f948a6be283fd
Rajan Kalra <rajan.kalra@hotmail.co.in>
2023-07-03 00:22:31 +0530

    Added min path sum with variable starting point

---
+++ grids/min-path-sum-variable-start.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/**
+ * 1e9 is 10^9 & is same as using INT_MAX.
+ * 1e9 doesn't need additional header to be included.
+*/
+int minPathSumVariableStartPointMemoizedUtil (vector<vector<int>> &mat, int rows, int cols, int i, int j, vector<vector<int>> &learn) {
+    // this condition is added based on movements we make i.e
+    // since any movement can only cross column boudary 
+    if (j < 0 || j >= cols)
+        return 1e9;
+    
+    // this condition is added since we need to stop the
+    // recursion as soon as we reach last row
+    if (i == rows-1)
+        return mat[i][j];
+
+    if (learn[i][j] != -1)
+        return learn[i][j];
+
+    int down = mat[i][j] + minPathSumVariableStartPointMemoizedUtil (mat, rows, cols, i+1, j, learn);
+    int downLeft = mat[i][j] + minPathSumVariableStartPointMemoizedUtil (mat, rows, cols, i+1, j-1, learn);
+    int downRight = mat[i][j] + minPathSumVariableStartPointMemoizedUtil (mat, rows, cols, i+1, j+1, learn);
+    
+    return learn[i][j] = min(down, min(downLeft, downRight));
+}
+
+int minPathSumVariableStartPointMemoized (vector<vector<int>> &mat) {
+    int rows = mat.size();
+    int cols = mat[0].size();
+    // we init i as 0 to always start from 0th row
+    int i = 0;
+
+    // extremely important step to init currentMin & min_sum with INT_MAX
+    int currentMin = INT_MAX, min_sum = INT_MAX;
+
+    vector<vector<int>> learn(rows, vector<int>(cols, -1));
+    for (int j = 0; j < cols; j++) {
+        currentMin = minPathSumVariableStartPointMemoizedUtil(mat, rows, cols, i, j, learn);
+        min_sum = min(currentMin, min_sum);
+    }
+
+    return min_sum;
+}
+
+/**
+ * 1e9 is 10^9 & is same as using INT_MAX.
+ * 1e9 doesn't need additional header to be included.
+*/
+int minPathSumVariableStartPointRecursiveUtil (vector<vector<int>> &mat, int rows, int cols, int i, int j) {
+    // this condition is added based on movements we make i.e
+    // since any movement can only cross column boudary 
+    if (j < 0 || j >= cols)
+        return 1e9;
+    
+    // this condition is added since we need to stop the
+    // recursion as soon as we reach last row
+    if (i == rows-1)
+        return mat[i][j];
+
+    int down = mat[i][j] + minPathSumVariableStartPointRecursiveUtil (mat, rows, cols, i+1, j);
+    int downLeft = mat[i][j] + minPathSumVariableStartPointRecursiveUtil (mat, rows, cols, i+1, j-1);
+    int downRight = mat[i][j] + minPathSumVariableStartPointRecursiveUtil (mat, rows, cols, i+1, j+1);
+    
+    return min(down, min(downLeft, downRight));
+}
+
+int minPathSumVariableStartPointRecursive (vector<vector<int>> &mat) {
+    int rows = mat.size();
+    int cols = mat[0].size();
+    // we init i as 0 to start always from 0th row
+    int i = 0;
+
+    // extremely important step to init currentMin & min_sum with INT_MAX
+    int currentMin = INT_MAX, min_sum = INT_MAX;
+
+    for (int j = 0; j < cols; j++) {
+        currentMin = minPathSumVariableStartPointRecursiveUtil(mat, rows, cols, i, j);
+        min_sum = min(currentMin, min_sum);
+    }
+
+    return min_sum;
+}
+
+int minPathSumVariableStartPoint (vector<vector<int>> &mat) {
+    return minPathSumVariableStartPointRecursive(mat);
+    // return minPathSumVariableStartPointMemoized(mat);
+}
+
+int main() {
+    vector<vector<int>> mat =   {
+                                    { 2, 1, 3 },
+                                    { 6, 5, 4 },
+                                    { 7, 8, 9 }
+                                };
+
+    cout << "Minimum path sum with variable starting points: " << minPathSumVariableStartPoint(mat) << endl;
+}
