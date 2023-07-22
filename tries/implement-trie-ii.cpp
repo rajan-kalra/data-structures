81c8b555117e86c5a6f7014100b520b18ab436c9
Rajan Kalra <rajan.kalra@hotmail.co.in>
2023-07-23 00:48:40 +0530

    Fixed indentation

--- tries/implement-trie-ii.cpp
+++ tries/implement-trie-ii.cpp
@@ -2,138 +2,137 @@
 using namespace std;
 
 struct Node {
-  Node* links[26] = { NULL };
-  int countEndsWith = 0;
-  int countPrefix = 0;
+    Node* links[26] = { NULL };
+    int countEndsWith = 0;
+    int countPrefix = 0;
 
-  // checks if the trie node corresponding to given character exists
-  bool isCharacterNodePresent(char ch) {
-    return (links[ch - 'a'] != NULL);
-  }
+    // checks if the trie node corresponding to given character exists
+    bool isCharacterNodePresent(char ch) {
+        return (links[ch - 'a'] != NULL);   
+    }
 
-  // to add new trie node corresponding to given character
-  void addCharacterNode(char ch, Node* node) {
-    links[ch - 'a'] = node;
-  }
+    // to add new trie node corresponding to given character
+    void addCharacterNode(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
 
-  // to get the trie node corresponding to given character
-  Node* getNextNode(char ch) {
-    return links[ch - 'a'];
-  }
-  
-  void incrementWordCount() {
-    countEndsWith++;
-  }
- 
-  void incrementPrefixCount() {
-    countPrefix++;
-  }
- 
-  void decrementWordCount() {
-    countEndsWith--;
-  }
- 
-  void decrementPrefixCount() {
-    countPrefix--;
-  }
- 
-  int getWordCount() {
-    return countEndsWith;
-  }
- 
-  int getPrefixCount() {
-    return countPrefix;
-  }
+    // to get the trie node corresponding to given character
+    Node* getNextNode(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void incrementWordCount() {
+        countEndsWith++;
+    }
+
+    void incrementPrefixCount() {
+        countPrefix++;
+    }
+
+    void decrementWordCount() {
+        countEndsWith--;
+    }
+
+    void decrementPrefixCount() {
+        countPrefix--;
+    }
+
+    int getWordCount() {
+        return countEndsWith;
+    }
+
+    int getPrefixCount() {
+        return countPrefix;
+    }
 };
  
 class Trie {
-  private:
-    Node* root;
+    private:
+        Node* root;
  
-  public:
+    public:
     /* Initialize your data structure here */
     Trie() {
-      root = new Node();
+        root = new Node();
     }
  
     /* Inserts a word into the trie */
     void insert(string word) {
-      Node* node = root;
-      for (int i = 0; i < word.length(); i++) {
-        if (node->isCharacterNodePresent(word[i]) == false) {
-          node->addCharacterNode(word[i], new Node());
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (node->isCharacterNodePresent(word[i]) == false) {
+                node->addCharacterNode(word[i], new Node());
+            }
+
+            node = node->getNextNode(word[i]);
+            node->incrementPrefixCount();
         }
-
-        node = node->getNextNode(word[i]);
-        node->incrementPrefixCount();
-      }
-
-      node->incrementWordCount();
+        
+        node->incrementWordCount();
     }
  
     int countWordsEqualTo(string &word) {
-      Node* node = root;
-      for (int i = 0; i < word.length(); i++) {
-        if (node->isCharacterNodePresent(word[i])) {
-          node = node->getNextNode(word[i]);
-        } else {
-          return 0;
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (node->isCharacterNodePresent(word[i])) {
+                node = node->getNextNode(word[i]);
+            } else {
+                return 0;
+            }
         }
-      }
-
-      return node->getWordCount();
+        
+        return node->getWordCount();
     }
 
     int countWordsStartingWith(string & word) {
-      Node * node = root;
-      for (int i = 0; i < word.length(); i++) {
-        if (node->isCharacterNodePresent(word[i])) {
-          node = node -> getNextNode(word[i]);
-        } else {
-          return 0;
+        Node * node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (node->isCharacterNodePresent(word[i])) {
+                node = node -> getNextNode(word[i]);
+            } else {
+                return 0;
+            }
         }
-      }
 
-      return node->getPrefixCount();
+        return node->getPrefixCount();
     }
  
     void erase(string & word) {
-      Node * node = root;
-      for (int i = 0; i < word.length(); i++) {
-        if (node->isCharacterNodePresent(word[i])) {
-          node = node->getNextNode(word[i]);
-          node->decrementPrefixCount();
-        } else {
-          return;
+        Node * node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (node->isCharacterNodePresent(word[i])) {
+                node = node->getNextNode(word[i]);
+                node->decrementPrefixCount();
+            } else {
+                return;
+            }
         }
-      }
-
-      node->decrementWordCount();
-	  }
+        node->decrementWordCount();
+    }
 };
 
 int main() {
-  // initialize trie
-  Trie T;
+    // initialize trie
+    Trie T;
 
-  T.insert("apple");
-  T.insert("apple");
-  T.insert("apps");
-  T.insert("apps");
+    T.insert("apple");
+    T.insert("apple");
+    T.insert("apps");
+    T.insert("apps");
 
-  string word1 = "apps";
-  cout << "Count of words equal to " << word1 << ": " << T.countWordsEqualTo(word1) << endl;
+    string word1 = "apps";
+    cout << "Count of words equal to " << word1 << ": " << T.countWordsEqualTo(word1) << endl;
 
-  string word2 = "abc";
-  cout << "Count of words equal to " << word2 << ": " << T.countWordsEqualTo(word2) << endl;
+    string word2 = "abc";
+    cout << "Count of words equal to " << word2 << ": " << T.countWordsEqualTo(word2) << endl;
 
-  string word3 = "ap";
-  cout << "Count of words starting with " << word3 << ": " << T.countWordsStartingWith(word3) << endl;
+    string word3 = "ap";
+    cout << "Count of words starting with " << word3 << ": " << T.countWordsStartingWith(word3) << endl;
 
-  string word4 = "appl";
-  cout << "Count of words starting with " << word4 << ": " << T.countWordsStartingWith(word4) << endl;
+    string word4 = "appl";
+    cout << "Count of words starting with " << word4 << ": " << T.countWordsStartingWith(word4) << endl;
 
-  T.erase(word1);
-  cout << "Count of words equal to " << word1 << ": " << T.countWordsEqualTo(word1) << endl;
-  return 0;
+    T.erase(word1);
+    cout << "Count of words equal to " << word1 << ": " << T.countWordsEqualTo(word1) << endl;
+    return 0;
 }
