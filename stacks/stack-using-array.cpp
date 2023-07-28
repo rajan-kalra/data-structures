284e0a1e77b85fd75486cef41a2a621ad6e596d6
Rajan Kalra <rajan.kalra@hotmail.co.in>
2023-07-29 00:12:18 +0530

    Added stack implementation using array & queue

--- stacks/stack-using-array.cpp
+++ stacks/stack-using-array.cpp
@@ -2,86 +2,93 @@
 using namespace std;
 
 class Stack {
-  private:
-    int top;
-    int capacity;
-    int* arr;
+    private:
+        int top;
+        int capacity;
+        int* arr;
  
-  public:
-    // default constructor
-    Stack() {
-      top = -1;
-      capacity = 1000;
-      arr = new int[capacity];
-    }
+    public:
+        // default constructor
+        Stack() {
+            top = -1;
+            capacity = 1000;
+            arr = new int[capacity];
+        }
 
-    // parameterized constructor
-    Stack(int capacity) {
-      top = -1;
-      this->capacity = capacity;
-      arr = new int[capacity];
-    }
+        // parameterized constructor
+        Stack(int capacity) {
+            top = -1;
+            this->capacity = capacity;
+            arr = new int[capacity];
+        }
 
-    bool push(int x);
-    int pop();
-    int peek();
-    bool isEmpty();
+        bool push(int x);
+        int pop();
+        int peek();
+        bool isEmpty();
 };
  
 bool Stack::push(int x) {
-  // edge case
-  if (top >= (capacity - 1)) {
-    cout << "Stack Overflow!";
-    return false;
-  }
+    /**
+     * Edge Case: since the array has 0 based indexing, max
+     * elements this array can accomodate is capacity-1
+     */
+    if (top >= (capacity - 1)) {
+        cout << "Stack Overflow!";
+        return false;
+    }
 
-  arr[++top] = x;
-  cout << x << " pushed into stack\n";
-  return true;
+    arr[++top] = x;
+    cout << x << " pushed into stack\n";
+    return true;
 }
  
 int Stack::pop() {
-  if (top < 0) {
-    cout << "Stack Underflow!";
-    return 0;
-  }
-  
-  int x = arr[top--];
-  return x;
+    /**
+     * Edge Case: since stack is initialised with top = -1, we
+     * use the same condition to find if stack has no element
+     */
+    if (top < 0) {
+        cout << "Stack Underflow!";
+        return 0;
+    }
+
+    int x = arr[top--];
+    return x;
 }
 
 int Stack::peek() {
-  if (top < 0) {
-    cout << "Stack is Empty";
-    return 0;
-  }
+    if (top < 0) {
+        cout << "Stack is Empty";
+        return 0;
+    }
 
-  int x = arr[top];
-  return x;
+    int x = arr[top];
+    return x;
 }
  
 bool Stack::isEmpty() {
-  return (top < 0);
+    return (top < 0);
 }
 
 int main() {
-  class Stack s;
-  s.push(10);
-  s.push(20);
-  s.push(30);
-  cout << s.pop() << " Popped from stack\n";
-   
-  //print top element of stack after popping
-  cout << "Top element is : " << s.peek() << endl;
-  
-  //print all elements in stack :
-  cout <<"Elements present in stack : ";
-  while(!s.isEmpty()) {
-    // print top element in stack
-    cout << s.peek() <<" ";
-    // remove top element in stack
-    s.pop();
-  }
-  
-  return 0;
+    class Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout << s.pop() << " Popped from stack\n";
+
+    //print top element of stack after popping
+    cout << "Top element is : " << s.peek() << endl;
+
+    //print all elements in stack :
+    cout <<"Elements present in stack : ";
+    while(!s.isEmpty()) {
+        // print top element in stack
+        cout << s.peek() <<" ";
+        // remove top element in stack
+        s.pop();
+    }
+
+    return 0;
 }
--- stacks/stack-using-single-queue.cpp
+++ stacks/stack-using-single-queue.cpp
@@ -4,45 +4,56 @@
 using namespace std;
 
 class Stack {
-  queue<int> q;
-  public:
-    void push(int x) {
-      // get the current size of the queue before pushing new element
-      int s = q.size();
-      cout << "Pushed element: " << x << endl;
-      q.push(x);
+    private:
+        queue<int> q;
 
-      // pop & push the 'size' no of elements
-      for (int i = 0; i < s; i++) {
-        q.push(q.front());
-        q.pop();
-      }
-    }
+    public:
+        void push(int x) {
+            /**
+             * get the current size of the queue before pushing
+             * any new element
+             */
+            int s = q.size();
 
-    int pop() {
-      int n = q.front();
-      q.pop();
-      return n;
-    }
+            cout << "Pushed element: " << x << endl;
+            q.push(x);
 
-    int top() {
-      return q.front();
-    }
+            /**
+             * pop & push the 's' no of elements where s is the
+             * size of queue before pushing this element
+             */
+            for (int i = 0; i < s; i++) {
+                q.push(q.front());
+                q.pop();
+            }
+        }
 
-    int size() {
-      return q.size();
-    }
+        int pop() {
+            int n = q.front();
+            q.pop();
+            
+            return n;
+        }
+
+        int top() {
+            return q.front();
+        }
+
+        int size() {
+            return q.size();
+        }
 };
 
 int main() {
-  Stack s;
-  s.push(3);
-  s.push(2);
-  s.push(4);
-  s.push(1);
-  cout << "Top of the stack: " << s.top() << endl;
-  cout << "Size of the stack: " << s.size() << endl;
-  cout << "Popped element: " << s.pop() << endl;
-  cout << "Top of the stack: " << s.top() << endl;
-  cout << "Size of the stack: " << s.size();
+    Stack s;
+    s.push(3);
+    s.push(2);
+    s.push(4);
+    s.push(1);
+
+    cout << "Top of the stack: " << s.top() << endl;
+    cout << "Size of the stack: " << s.size() << endl;
+    cout << "Popped element: " << s.pop() << endl;
+    cout << "Top of the stack: " << s.top() << endl;
+    cout << "Size of the stack: " << s.size();
 }
