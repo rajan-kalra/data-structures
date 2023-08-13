46caab62fbd81e634d6189c671943c8c769d86be
Rajan Kalra <rajan.kalra@hotmail.co.in>
2023-08-14 00:54:54 +0530

    Added comments

--- graphs/dist-of-nearest-1.cpp
+++ graphs/dist-of-nearest-1.cpp
@@ -5,82 +5,90 @@
 using namespace std;
 
 class Solution {
-  public:
-  //Function to find the distance of nearest 1 in the grid for each cell.
-	vector<vector<int>> nearest(vector<vector<int>> grid) {
-    int noOfRows = grid.size();
-    int noOfCols = grid[0].size();
+    public:
+        // Function to find the distance of nearest 1 in the grid for each cell
+        vector<vector<int>> nearest(vector<vector<int>> grid) {
+            int noOfRows = grid.size();
+            int noOfCols = grid[0].size();
 
-    // visited and distance matrix
-    vector<vector<int>> visited(noOfRows, vector<int>(noOfCols, 0));
-    vector<vector<int>> distance(noOfRows, vector<int>(noOfCols, 0));
+            // visited and distance matrix
+            vector<vector<int>> visited(noOfRows, vector<int>(noOfCols, 0));
+            vector<vector<int>> distance(noOfRows, vector<int>(noOfCols, 0));
 
-    // <coordinates, steps>
-    queue<pair<pair<int,int>, int>> q;
+            // {{row,col}, steps}
+            queue<pair<pair<int,int>, int>> q;
 
-    // traverse the matrix
-    for (int i = 0; i < noOfRows; i++) {
-      for (int j = 0; j < noOfCols; j++) {
-        // start BFS if cell contains 1
-        if (grid[i][j] == 1) {
-          // adding <coordinates, steps>
-          q.push({{i,j}, 0}); 
-          visited[i][j] = 1; 
-        } else {
-          // mark unvisited 
-          visited[i][j] = 0; 
-        }
-      }
-    }
+            /**
+             * Traversing the grid to find all 1s & push them to
+             * the queue. We need to run BFS on these 1s to find
+             * the min distance of 1s from all others cells
+             */
+            for (int row = 0; row < noOfRows; row++) {
+                for (int col = 0; col < noOfCols; col++) {
+                    // start BFS if cell contains 1
+                    if (grid[row][col] == 1) {
+                        // adding <coordinates, steps>
+                        q.push({{row,col}, 0}); 
+                        visited[row][col] = 1; 
+                    } else {
+                        // mark unvisited 
+                        visited[row][col] = 0; 
+                    }
+                }
+            }
 
-    int moveRow[] = {-1, 0, +1, 0}; 
-    int moveCol[] = {0, +1, 0, -1}; 
+            int moveRow[] = {-1, 0, +1, 0}; 
+            int moveCol[] = {0, +1, 0, -1}; 
     
-    // traverse till queue becomes empty
-    while (q.empty() == false) {
-      // get coordinates
-      int row = q.front().first.first; 
-      int col = q.front().first.second;
+            // traverse till queue becomes empty
+            while (q.empty() == false) {
+                // get coordinates
+                int row = q.front().first.first; 
+                int col = q.front().first.second;
 
-      // get number of steps
-      int steps = q.front().second; 
-      q.pop();
+                // get number of steps
+                int steps = q.front().second; 
+                q.pop();
 
-      distance[row][col] = steps; 
-      // for all 4 neighbours
-      for (int i = 0; i < 4; i++) {
-          int nrow = row + moveRow[i]; 
-          int ncol = col + moveCol[i]; 
-          // check for valid unvisited cell
-          if ((nrow >= 0 && nrow < noOfRows) && (ncol >= 0 && ncol < noOfCols) && (visited[nrow][ncol] == 0)) {
-              visited[nrow][ncol] = 1; 
-              // we increment current steps by 1
-              q.push({{nrow, ncol}, steps + 1});  
-          }
-      }
-    }
+                distance[row][col] = steps; 
 
-    // return distance matrix
-    return distance; 
-	}
+                // for all 4 neighbours
+                for (int i = 0; i < 4; i++) {
+                    int newRow = row + moveRow[i]; 
+                    int newCol = col + moveCol[i]; 
+                    // check for valid unvisited cell
+                    if ((newRow >= 0 && newRow < noOfRows) &&
+                        (newCol >= 0 && newCol < noOfCols) &&
+                        (visited[newRow][newCol] == 0)) {
+                        visited[newRow][newCol] = 1; 
+                        // we increment current steps by 1
+                        q.push({{newRow, newCol}, steps + 1});  
+                    }
+                }
+            }
+
+            // return distance matrix
+            return distance; 
+	    }
 };
 
 int main(){
-  vector<vector<int>> grid  {
-                              {0,1,1,0},
-                              {1,1,0,0},
-                              {0,0,1,1}
-                            };
+    vector<vector<int>> grid    {
+                                    { 0,1,1,0 },
+                                    { 1,1,0,0 },
+                                    { 0,0,1,1 }
+                                };
 
-  Solution obj;
-  vector<vector<int>> ans = obj.nearest(grid);
-  // print the ans matrix
-  for (auto i : ans) {
-    for (auto j : i) {
-      cout << j << " ";
+    Solution obj;
+    vector<vector<int>> ans = obj.nearest(grid);
+
+    // print the ans matrix
+    for (auto vector : ans) {
+        for (auto element : vector) {
+            cout << element << " ";
+        }
+        cout << "\n";
     }
-    cout << "\n";
-  }
 
-  return 0;
+    return 0;
 }
--- graphs/surrounded-regions.cpp
+++ graphs/surrounded-regions.cpp
@@ -4,89 +4,100 @@
 using namespace std;
 
 class Solution {
-  private:
-    void dfs (int row, int col, vector<vector<int>> &visited, vector<vector<char>> &mat, int moveRow[], int moveCol[]) {
-      visited[row][col] = 1;
-      int noOfRows = mat.size();
-      int noOfCols = mat[0].size();
-      
-      // check for top, right, bottom, left 
-      for (int i = 0; i < 4; i++) {
-        int nrow = row + moveRow[i];
-        int ncol = col + moveCol[i]; 
-        // check for valid coordinates and unvisited Os
-        if ((nrow >= 0 && nrow < noOfRows) && (ncol >= 0 && ncol < noOfCols)  && (!visited[nrow][ncol] && mat[nrow][ncol] == 'O')) {
-          dfs(nrow, ncol, visited, mat, moveRow, moveCol);
+    private:
+        void dfs (int row, int col, vector<vector<int>> &visited, vector<vector<char>> &mat, int moveRow[], int moveCol[]) {
+            visited[row][col] = 1;
+            int noOfRows = mat.size();
+            int noOfCols = mat[0].size();
+            
+            // check for top, right, bottom, left 
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + moveRow[i];
+                int ncol = col + moveCol[i]; 
+                // check for valid coordinates and unvisited Os
+                if ((nrow >= 0 && nrow < noOfRows) &&
+                    (ncol >= 0 && ncol < noOfCols) &&
+                    (!visited[nrow][ncol] && mat[nrow][ncol] == 'O')) {
+                    dfs(nrow, ncol, visited, mat, moveRow, moveCol);
+                }
+            }
         }
-      }
-    }
 
-  public:
-    vector<vector<char>> fill(int noOfRows, int noOfCols, vector<vector<char>> inputGrid) {
-      int moveRow[] = {-1, 0, +1, 0};
-      int moveCol[] = {0, 1, 0, -1};
+    public:
+        vector<vector<char>> fill(int noOfRows, int noOfCols, vector<vector<char>> inputGrid) {
+            int moveRow[] = { -1, 0, +1,  0 };
+            int moveCol[] = {  0, 1,  0, -1 };
 
-      vector<vector<int>> visited(noOfRows, vector<int>(noOfCols, 0));
-      
-      // traverse first row and last row 
-      for(int j = 0 ; j < noOfCols; j++) {
-          // for boundary rows, check for unvisited Os
-          // first row 
-          if(inputGrid[0][j] == 'O' && visited[0][j] == 0) {
-            dfs(0, j, visited, inputGrid, moveRow, moveCol); 
-          }
-          
-          // last row 
-          if(inputGrid[noOfRows-1][j] == 'O' && visited[noOfRows-1][j] == 0) {
-            dfs(noOfRows-1, j, visited, inputGrid, moveRow, moveCol); 
-          }
-      }
+            vector<vector<int>> visited(noOfRows, vector<int>(noOfCols, 0));
         
-      for (int i = 0; i < noOfRows; i++) {
-          // for boundary columns, check for unvisited Os
-          // first column 
-          if (inputGrid[i][0] == 'O' && visited[i][0] == 0) {
-            dfs(i, 0, visited, inputGrid, moveRow, moveCol); 
-          }
-          
-          // last column
-          if (inputGrid[i][noOfCols-1] == 'O' && visited[i][noOfCols-1] == 0) {
-            dfs(i, noOfCols-1, visited, inputGrid, moveRow, moveCol); 
-          }
-      }
+            /**
+             * Traversing first and last row
+             * We apply dfs on every 0 found on boundary
+             */ 
+            for (int col = 0; col < noOfCols; col++) {
+                // first row 
+                if (inputGrid[0][col] == 'O' && visited[0][col] == 0) {
+                    dfs(0, col, visited, inputGrid, moveRow, moveCol); 
+                }
+                
+                // last row 
+                if(inputGrid[noOfRows-1][col] == 'O' && visited[noOfRows-1][col] == 0) {
+                    dfs(noOfRows-1, col, visited, inputGrid, moveRow, moveCol); 
+                }
+            }
+            
+            /**
+             * Traversing first and last column
+             * We apply dfs on every 0 found on boundary
+             */ 
+            for (int row = 0; row < noOfRows; row++) {
+                // first column 
+                if (inputGrid[row][0] == 'O' && visited[row][0] == 0) {
+                    dfs(row, 0, visited, inputGrid, moveRow, moveCol); 
+                }
+                
+                // last column
+                if (inputGrid[row][noOfCols-1] == 'O' && visited[row][noOfCols-1] == 0) {
+                    dfs(row, noOfCols-1, visited, inputGrid, moveRow, moveCol); 
+                }
+            }
 
-      // if unvisited O then convert to X
-      for (int i = 0; i < noOfRows; i++) {
-          for (int j = 0; j < noOfCols; j++) {
-              if (visited[i][j] == 0 && inputGrid[i][j] == 'O') 
-                inputGrid[i][j] = 'X'; 
-          }
-      }
-      
-      return inputGrid; 
-    }
+            /**
+             * At this point if any cell in the grid is left
+             * with 0, it infers that it wasn't reachable from
+             * any 0 on the boundary& thus can be transformed to X
+             */
+            for (int i = 0; i < noOfRows; i++) {
+                for (int j = 0; j < noOfCols; j++) {
+                    if (visited[i][j] == 0 && inputGrid[i][j] == 'O') 
+                        inputGrid[i][j] = 'X'; 
+                }
+            }
+            
+            return inputGrid; 
+        }
 };
 
-int main(){
-  vector<vector<char>> inputGrid  {
-                                    {'X', 'X', 'X', 'X'}, 
-                                    {'X', 'O', 'X', 'X'}, 
-                                    {'X', 'O', 'O', 'X'}, 
-                                    {'X', 'O', 'X', 'X'}, 
-                                    {'X', 'X', 'O', 'O'}
-                                  };
+int main() {
+    vector<vector<char>> inputGrid  {
+                                        {'X', 'X', 'X', 'X'}, 
+                                        {'X', 'O', 'X', 'X'}, 
+                                        {'X', 'O', 'O', 'X'}, 
+                                        {'X', 'O', 'X', 'X'}, 
+                                        {'X', 'X', 'O', 'O'}
+                                    };
 
-  Solution ob;
-  int noOfRows = inputGrid.size();
-  int noOfCols = inputGrid[0].size();
-  vector<vector<char>> ans = ob.fill(noOfRows, noOfCols, inputGrid);
+    Solution ob;
+    int noOfRows = inputGrid.size();
+    int noOfCols = inputGrid[0].size();
+    vector<vector<char>> ans = ob.fill(noOfRows, noOfCols, inputGrid);
 
-  for (int i = 0; i < noOfRows; i++) {
-    for (int j = 0; j < noOfCols; j++) {
-      cout << ans[i][j] << " ";
+    for (int row = 0; row < noOfRows; row++) {
+        for (int col = 0; col < noOfCols; col++) {
+            cout << ans[row][col] << " ";
+        }
+        cout<<"\n";
     }
-    cout<<"\n";
-  }
 
-  return 0;
+    return 0;
 }
