d9bba0f19cba38ef5ec6a2641a8d65bda7403274
Rajan Kalra <rajan.kalra@hotmail.co.in>
2023-08-13 14:59:35 +0530

    Added comments

--- graphs/flood-fill.cpp
+++ graphs/flood-fill.cpp
@@ -3,63 +3,79 @@
 using namespace std;
  
 class Solution {
-  private:
-    void dfs(int currRow, int col, vector<vector<int>> &ans, vector<vector<int>> &image, int newColor, int moveRow[], int moveCol[], int iniColor) {
-      // color with new color
-      ans[currRow][col] = newColor; 
-      int n = image.size();
-      int m = image[0].size();
+    private:
+        void dfs(int currRowIndex, int currColIndex, vector<vector<int>> &floodFilledImage, vector<vector<int>> &inputImage, int newColor, int moveRow[], int moveCol[], int iniColor) {
+            // color with new color
+            floodFilledImage[currRowIndex][currColIndex] = newColor; 
+            int rows = inputImage.size();
+            int cols = inputImage[0].size();
 
-      // there are exactly 4 neighbours
-      for (int i = 0; i < 4; i++) {
-        int nrow = currRow + moveRow[i];
-        int ncol = col + moveCol[i];
-        
-        /**
-         * conditions to be checked to apply dfs on new cell:
-         * 1. New row is valid
-         * 2. New col is valid
-         * 3. New cell has initial color
-         * 4. New cell doesn't have new color
-         */
-        if((nrow >= 0 && nrow < n) && (ncol >= 0 && ncol < m) && (image[nrow][ncol] == iniColor) && (ans[nrow][ncol] != newColor)) {
-            dfs(nrow, ncol, ans, image, newColor, moveRow, moveCol, iniColor); 
+            // there are exactly 4 neighbours
+            for (int i = 0; i < 4; i++) {
+            int newRowIndex = currRowIndex + moveRow[i];
+            int newColIndex = currColIndex + moveCol[i];
+            
+            /**
+             * conditions to be checked to apply dfs on new cell:
+             * 1. New row is valid
+             * 2. New col is valid
+             * 3. New cell has initial color
+             * 4. New cell doesn't have new color
+             */
+            if( (newRowIndex >= 0 && newRowIndex < rows) &&
+                (newColIndex >= 0 && newColIndex < cols) &&
+                (inputImage[newRowIndex][newColIndex] == iniColor) &&
+                (floodFilledImage[newRowIndex][newColIndex] != newColor)) {
+                dfs(newRowIndex, newColIndex, floodFilledImage, inputImage, newColor, moveRow, moveCol, iniColor); 
+            }
+            }
         }
-      }
-    }
 
-  public:
-    vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int newColor) {
-      // get initial color
-      int iniColor = image[sr][sc];
+    public:
+        vector<vector<int>> floodFill(vector<vector<int>> &inputImage, int sr, int sc, int newColor) {
+            // get initial color
+            int iniColor = inputImage[sr][sc];
 
-      // creating the copy of the input
-      vector<vector<int>> ans = image;
+            // creating the copy of the input
+            vector<vector<int>> floodFilledImage = inputImage;
 
-      // delta row and delta column for neighbours
-      int moveRow[] = { -1,  0, +1,  0 };
-      int moveCol[] = {  0, +1,  0, -1 };
+            // delta row and delta column for neighbours
+            int moveRow[] = { -1,  0, +1,  0 };
+            int moveCol[] = {  0, +1,  0, -1 };
 
-      dfs(sr, sc, ans, image, newColor, moveRow, moveCol, iniColor); 
-      return ans; 
-    }
+            dfs(sr, sc, floodFilledImage, inputImage, newColor, moveRow, moveCol, iniColor); 
+            return floodFilledImage; 
+        }
 };
- 
-int main(){
-  vector<vector<int>> image {
-                              {1,1,1},
-                              {1,1,0},
-                              {1,0,1}
-                            };
- 
-  // sr = 1, sc = 1, newColor = 2
-  Solution obj;
-  vector<vector<int>> ans = obj.floodFill(image, 1, 1, 2);
-  for(auto i: ans){
-    for(auto j: i)
-      cout << j << " ";
-    cout << "\n";
-  }
 
-  return 0;
+void printMatrix (vector<vector<int> > &image) {
+    int rows = image.size();
+    int cols = image[0].size();
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << image[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
+
+int main(){
+    vector<vector<int>> inputImage {
+                                {1,1,1},
+                                {1,1,0},
+                                {1,0,1}
+                            };
+
+    // sr = 1, sc = 1, newColor = 2
+    Solution obj;
+    vector<vector<int>> floodFilledImage = obj.floodFill(inputImage, 1, 1, 2);
+    cout << "Matrix before flood fill: " << endl;
+    printMatrix(inputImage);
+
+    cout << "Matrix after flood fill: " << endl;
+    printMatrix(floodFilledImage);
+
+    return 0;
 }
--- graphs/rotting-oranges.cpp
+++ graphs/rotting-oranges.cpp
@@ -5,94 +5,127 @@
 using namespace std;
  
 class Solution {
-  public:
-    //Function to find minimum time required to rot all oranges
-    int orangesRotting(vector<vector<int>> &grid) {
-      // figure out the grid size
-      int noOfRows = grid.size();
-      int noOfCols = grid[0].size();
+    public:
+        int rottenOranges(vector<vector<int>> &grid) {
+        // find out the grid size
+        int noOfRows = grid.size();
+        int noOfCols = grid[0].size();
 
-      // store {{row, column}, time}
-      queue<pair<pair<int,int>, int>> q;
-      int visited[noOfRows][noOfCols];
-      int countOfFreshOranges = 0;
- 
-      /**
-       * Iterating through the entire grid to find out count of
-       * fresh oranges & creating the visited matrix
-       **/
-      for (int i = 0; i < noOfRows; i++) {
-        for (int j = 0; j < noOfCols; j++) {
-          // if cell contains rotten orange
-          if (grid[i][j] == 2) {
-            q.push({{i, j}, 0}); 
-            // mark as visited (rotten) in visited array
-            visited[i][j] = 2;
-          // if not rotten
-          } else {
-            visited[i][j] = 0;
-          }
+        /**
+         * Queue has object {{row, column}, time}.
+         * We need to know which unit time does the current
+         * orange belong to. Using this corresponding time
+         * for each orange we find out the next unit time
+         * for each orange
+         */
+        queue<pair<pair<int,int>, int>> q;
+        int visited[noOfRows][noOfCols];
+        int countOfFreshOranges = 0;
 
-          // count fresh oranges
-          if (grid[i][j] == 1) {
-            countOfFreshOranges++;
-          }
+        /**
+         * Iterating through the all the cells of grid to:
+         * 1. Populate the visited matrix & push all already rotten
+         * oranges to queue
+         * 2. Find count of fresh oranges
+         **/
+        for (int i = 0; i < noOfRows; i++) {
+            for (int j = 0; j < noOfCols; j++) {
+                /**
+                 * if current cell contains rotten orange, add
+                 * it to queue with starting unit time as 0 &
+                 * mark it as rotten in visited array
+                 */
+                if (grid[i][j] == 2) {
+                    q.push({{i, j}, 0}); 
+                    // mark as visited (rotten) in visited array
+                    visited[i][j] = 2;
+                // if not rotten, mark it as non-rotten in visited array
+                } else {
+                    visited[i][j] = 0;
+                }
+
+                /**
+                 * Count fresh oranges, to be used later to find out if
+                 * any fresh orange is left by comparing with no of rotten
+                 * oranges
+                 */
+                if (grid[i][j] == 1) {
+                    countOfFreshOranges++;
+                }
+            }
         }
-      }
  
-      int tm = 0;
-      // delta movement row and column
-      int moveRow[] = {-1, 0, +1, 0};
-      int moveCol[] = {0, 1, 0, -1};
-      int count = 0;
- 
-      // bfs traversal (until the queue becomes empty)
-      while (q.empty() == false) {
-        int currRow = q.front().first.first;
-        int currCol = q.front().first.second;
-        int unitTime = q.front().second;
-        tm = max(tm, unitTime);
-        q.pop();
+        int maxTime = 0;
+        // delta movement row and column
+        int moveRow[] = { -1, 0, +1, 0 };
+        int moveCol[] = {  0, 1, 0, -1 };
+        int count = 0;
 
-        // exactly 4 neighbours 
-        for (int i = 0; i < 4; i++) {
-          // neighbouring row and column
-          int newRow = currRow + moveRow[i];
-          int newCol = currCol + moveCol[i];
-          /**
-           * conditions to be checked to apply dfs on new cell:
-           * 1. New row is valid
-           * 2. New col is valid
-           * 3. New cell is not visited in visited matrix
-           * 4. New cell has a fresh orange
-           */
-          if ((newRow >= 0 && newRow < noOfRows) && (newCol >= 0 && newCol < noOfCols) && (visited[newRow][newCol] == 0) && (grid[newRow][newCol] == 1)) {
-            // push in queue with timer increased
-            q.push({{newRow, newCol}, unitTime + 1}); 
-            // mark as rotten
-            visited[newRow][newCol] = 2;
-            count++;
-          }
+        /**
+         * bfs traversal (until the queue becomes empty)
+         * To note, we dont need the bfs loop in this case as we
+         * already have nodes / oranges to be processed in the
+         * queue.
+         * We essentially need to process only the oranges which
+         * are rotten to start with & the oranges that will get
+         * rotten in subsequent time due to already rotten oranges
+         */
+        while (q.empty() == false) {
+            int currRowIndex = q.front().first.first;
+            int currColIndex = q.front().first.second;
+            int currUnitTime = q.front().second;
+
+            maxTime = max(maxTime, currUnitTime);
+            q.pop();
+
+            // current orange can rot 4 neighbours in 4 directions
+            for (int i = 0; i < 4; i++) {
+                // neighbouring row and column
+                int newRowIndex = currRowIndex + moveRow[i];
+                int newColIndex = currColIndex + moveCol[i];
+
+                /**
+                 * Conditions to check while applying bfs on new neighboring cell:
+                 * 1. New row is valid
+                 * 2. New col is valid
+                 * 3. New cell is not visited in visited matrix
+                 * 4. New cell has a fresh orange
+                 */
+                if ((newRowIndex >= 0 && newRowIndex < noOfRows) &&
+                    (newColIndex >= 0 && newColIndex < noOfCols) &&
+                    (visited[newRowIndex][newColIndex] == 0) &&
+                    (grid[newRowIndex][newColIndex] == 1)) {
+                    // push in queue with timer increased
+                    q.push({{newRowIndex, newColIndex}, currUnitTime + 1}); 
+                    // mark as rotten
+                    visited[newRowIndex][newColIndex] = 2;
+                    count++;
+                }
+            }
         }
-      }
- 
-      // if all oranges are not rotten
-      if (count != countOfFreshOranges)
-        return -1;
-  
-      return tm;
+
+        // if all oranges are not rotten
+        if (count != countOfFreshOranges)
+            return -1;
+
+        return maxTime;
     }
 };
  
 int main() {
-  vector<vector<int>> grid	{
-                              {0, 1, 2},
-                              {0, 1, 2},
-                              {2, 1, 1}
-                            };
+    /**
+     * 0 - empty cell
+     * 1 - fresh orange
+     * 2 - rotten orange
+     */
+    vector<vector<int>> grid    {
+                                    {0, 1, 2},
+                                    {0, 1, 1},
+                                    {2, 1, 1}
+                                };
 
-  Solution obj;
-  int ans = obj.orangesRotting(grid);
-  cout << ans << "\n";
-  return 0;
+    Solution obj;
+    int ans = obj.rottenOranges(grid);
+    cout << ans << "\n";
+    return 0;
 }
