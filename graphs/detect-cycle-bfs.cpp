f5d4defe98d1ba4dd8e1c2abe6822e1f3b40449a
Rajan Kalra <rajan.kalra@hotmail.co.in>
2023-08-13 23:12:16 +0530

    Fixed comments

--- graphs/detect-cycle-bfs.cpp
+++ graphs/detect-cycle-bfs.cpp
@@ -3,70 +3,78 @@
 using namespace std;
  
 class Solution {
-  public:
-    bool checkForCycle(int sourceNode, int V, vector<int> adjList[], vector<int> &visited) {
-      // Create a queue for BFS
-      // this queue has the node & its parent
-      queue<pair<int, int>> q;
+    public:
+        bool checkForCycle(int sourceNode, int V, vector<int> adjList[], vector<bool> &visited) {
+            /**
+             * Create a queue for BFS
+             * This queue has node & its parent
+             */
+            queue<pair<int, int>> q;
 
-      visited[sourceNode] = true;
+            visited[sourceNode] = true;
 
-      // first node has parent as -1
-      q.push({sourceNode, -1});
+            // first node has parent as -1
+            q.push({sourceNode, -1});
 
-      while (q.empty() == false) {
-        int node = q.front().first;
-        int parent = q.front().second;
-        q.pop();
+            while (q.empty() == false) {
+                int node = q.front().first;
+                int parent = q.front().second;
+                q.pop();
 
-        for (auto it : adjList[node]) {
-          if (!visited[it]) {
-            visited[it] = true;
-            q.push({it, node});
-          } else if (parent != it) {
-            return true;
-          }
+                for (auto neighborNode : adjList[node]) {
+                    if (visited[neighborNode] == false) {
+                        visited[neighborNode] = true;
+                        q.push({neighborNode, node});
+                    // A node can be already visited but if a node
+                    // is already visited & its parent is not 
+                    } else if (parent != neighborNode) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
-      }
 
-      return false;
-    }
- 
-    bool isCycle(int V, vector<int> adjList[]) {
-      vector<int> visited(V - 1, 0);
-      // traverse the entire adjacency list to check
-      // no node is part of any cycle
-      for (int i = 1; i <= V; i++) {
-        if (visited[i] == 0) {
-          if (checkForCycle(i, V, adjList, visited))
-            return true;
+        bool isCyclePresent(int V, vector<int> adjList[]) {
+            vector<bool> visited(V - 1, false);
+
+            /**
+             * Iterate through all the nodes to ensure there 
+             * is no cycle present in any component if multiple
+             * components are present
+             */
+            for (int i = 1; i <= V; i++) {
+                if (visited[i] == 0) {
+                    if (checkForCycle(i, V, adjList, visited))
+                        return true;
+                }
+            }
+
+            return false;
         }
-      }
-
-      return false;
-    }
 };
  
 void addEdge(vector<int> adj[],int u,int v) {
-  adj[u].push_back(v);
-  adj[v].push_back(u);
+    adj[u].push_back(v);
+    adj[v].push_back(u);
 }
  
 int main() {
-  vector<int> adj[5];
+    vector<int> adj[5];
 
-  addEdge(adj,0,1);
-  addEdge(adj,0,2);
-  addEdge(adj,2,3);
-  addEdge(adj,1,3);
-  addEdge(adj,2,4);
+    addEdge(adj,0,1);
+    addEdge(adj,0,2);
+    addEdge(adj,2,3);
+    addEdge(adj,1,3);
+    addEdge(adj,2,4);
 
-  Solution obj;
-  int num=obj.isCycle(5, adj);
-  if(num==1)
-    cout<<"Yes"<<endl;
-  else
-    cout<<"No"<<endl;
+    Solution obj;
+    int num = obj.isCyclePresent(5, adj);
+    if (num == 1)
+        cout << "Cycle detected using BFS!" << endl;
+    else
+        cout << "Cycle not detected using BFS!" << endl;
 
-  return 0;
+    return 0;
 }
--- graphs/detect-cycle-dfs.cpp
+++ graphs/detect-cycle-dfs.cpp
@@ -4,65 +4,73 @@
 using namespace std;
  
 class Solution {
-  bool checkForCycle(int node, int parent, vector<int> &visited, vector<int> adj[]) {
-    visited[node] = 1;
-    for (auto it: adj[node]) {
-      if (visited[it] == 0) {
-        // here we pass the current node as parent
-        // of the node to be visited next
-        if (checkForCycle(it, node, visited, adj))
-          return true;
-      } else if (it != parent) {
-        return true;
-      }
-    }
+    private:
+        bool checkForCycle(int currentNode, int parent, vector<int> &visited, vector<int> adj[]) {
+            visited[currentNode] = 1;
+            for (auto neighborNode : adj[currentNode]) {
+                // current neighbor node is unvisited
+                if (visited[neighborNode] == 0) {
+                    // here we pass the current node as parent
+                    // of the node to be visited next
+                    if (checkForCycle(neighborNode, currentNode, visited, adj))
+                        return true;
+                // current neighbor node is visited
+                } else {
+                    if (neighborNode != parent) {
+                        return true;
+                    }
+                }
+            }
 
-    return false;
-  }
- 
-  public:
-    bool isCycle(int V, vector<int> adjList[]) {
-      vector<int> visited(V + 1, 0);
-      // traverse the entire adjacency list to find out
-      // if any node is part of a cycle
-      for (int i = 0; i < V; i++) {
-        if (visited[i] == 0) {
-          if (checkForCycle(i, -1, visited, adjList))
-            return true;
+            return false;
         }
-      }
+ 
+    public:
+        bool isCyclePresent(int V, vector<int> adjList[]) {
+            vector<int> visited(V+1, 0);
+            /**
+             * traverse complete adjacency list to find
+             * out if any node is part of a cycle
+             */
+            for (int currentNode = 0; currentNode < V; currentNode++) {
+                if (visited[currentNode] == 0) {
+                    if (checkForCycle(currentNode, -1, visited, adjList))
+                        return true;
+                }
+            }
 
-      return false;
-    }
+            return false;
+        }
 };
 
 int main() {
-  int noOfVertices = 5;
-  int noOfEdges = 5;
-  vector<int> adjList[noOfVertices];
- 
-  adjList[0].push_back(1);
-  adjList[1].push_back(0);
- 
-  adjList[1].push_back(2);
-  adjList[2].push_back(1);
- 
-  adjList[1].push_back(4);
-  adjList[4].push_back(1);
- 
-  adjList[4].push_back(3);
-  adjList[3].push_back(4);
- 
-  adjList[2].push_back(3);
-  adjList[3].push_back(2);
- 
-  Solution obj;
-  bool ans = obj.isCycle(noOfVertices, adjList);
-  if (ans) {
-    cout << "Cycle Detected";
-  } else {
-    cout << "No Cycle Detected";
-  }
- 
-  return 0;
+    int noOfVertices = 5;
+    int noOfEdges = 5;
+    vector<int> adjList[noOfVertices];
+
+    adjList[0].push_back(1);
+    adjList[1].push_back(0);
+
+    adjList[1].push_back(2);
+    adjList[2].push_back(1);
+
+    adjList[1].push_back(4);
+    adjList[4].push_back(1);
+
+    adjList[4].push_back(3);
+    adjList[3].push_back(4);
+
+    adjList[2].push_back(3);
+    adjList[3].push_back(2);
+
+    Solution obj;
+    bool ans = obj.isCyclePresent(noOfVertices, adjList);
+
+    if (ans) {
+        cout << "Cycle detected using DFS!";
+    } else {
+        cout << "Cycle not detected using DFS!";
+    }
+
+    return 0;
 }
