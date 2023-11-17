82fac5622b50bbe1e607edf8612e82298ea2eec8
Rajan Kalra <rajan.kalra@hotmail.co.in>
2023-11-18 00:39:58 +0530

    Modified longest-word-with-all-prefixes.cpp

--- tries/longest-word-with-all-prefixes.cpp
+++ tries/longest-word-with-all-prefixes.cpp
@@ -7,27 +7,28 @@ struct Node {
     bool flag;
 
     // checks if the trie node corresponding to given character exists
-    bool containsKey(char ch) {
+    bool isCharacterNodePresent(char ch) {
         return links[ch - 'a'];
     }
 
     // gets the trie node corresponding to given character
-    Node* getKeyNode(char ch) {
+    Node* getNextNode(char ch) {
         return links[ch - 'a'];
     }
 
     // to add new trie node corresponding to given character
-    void addKeyNode(char ch, Node* node) {
+    void addKeyNode(char ch) {
+        Node *node = new Node();
         links[ch - 'a'] = node;
     }
 
     // to make the node as end node
-    void setEnd () {
+    void setAsEndNode () {
         flag = true;
     }
 
     // check if node is end node
-    bool isEnd () {
+    bool isEndNode () {
         return flag;
     }
 };
@@ -37,50 +38,52 @@ class Trie {
         Node* root;
  
     public:
-    /* Initialize your data structure here */
-    Trie() {
-        root = new Node();
-    }
- 
-    /* Inserts a word into the trie */
-    void insert(string word) {
-        Node* node = root;
-        for (int i = 0; i < word.length(); i++) {
-            if (node->containsKey(word[i]) == false) {
-                node->addKeyNode(word[i], new Node());
-            }
-            node = node->getKeyNode(word[i]);
+        /* Initialize your data structure here */
+        Trie() {
+            root = new Node();
         }
-
-        node->setEnd();
-    }
-
-    bool checkIfAllPrefixExists (string word) {
-        bool f1 = true;
-        Node* node = root;
-
-        for (int i = 0; i < word.length(); i++) {
-            if (node->containsKey(word[i])) {
-                node = node->getKeyNode(word[i]);
-                if (node->isEnd() == false)
-                    return false;
+    
+        /* Inserts a word into the trie */
+        void insert(string word) {
+            Node* currNode = root;
+            for (int i = 0; i < word.length(); i++) {
+                if (currNode->isCharacterNodePresent(word[i]) == false) {
+                    currNode->addKeyNode(word[i]);
+                }
+                currNode = currNode->getNextNode(word[i]);
             }
 
-            return false;
+            currNode->setAsEndNode();
         }
 
-        return true;
-    }
+        bool checkIfAllPrefixExists (string word) {
+            bool f1 = true;
+            Node* currNode = root;
+
+            for (int i = 0; i < word.length(); i++) {
+                if (currNode->isCharacterNodePresent(word[i])) {
+                    currNode = currNode->getNextNode(word[i]);
+                    if (currNode->isEndNode() == false)
+                        return false;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
 };
 
 string completeString (int n, vector<string>& input) {
     Trie* obj = new Trie();
+    // creating trie from all the words in input vector
     for (string &word : input) {
         obj->insert(word);
     }
 
     string longest;
     for (auto &word: input) {
+        // for each word check if all the prefixes in the word exists
         if (obj->checkIfAllPrefixExists(word)) {
             if(word.size() > longest.size()) {
                 longest = word; 
