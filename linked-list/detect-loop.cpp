842e6f31f64945dc1d882aebf68b808d3dda0e91
Rajan Kalra <rajan.kalra@hotmail.co.in>
2023-11-20 20:50:24 +0530

    Modified detect loop

--- linked-list/detect-loop.cpp
+++ linked-list/detect-loop.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <unordered_set>
 using namespace std;
- 
+
 class Node {
-	public:
-	    int data;
+    public:
+        int data;
         Node *next;
         Node (int data) {
             this->data = data;
@@ -13,69 +13,71 @@ class Node {
 };
  
 Node* CreateNode (int data) {
-	Node *node = new Node(data);
-	return node;
+    Node *node = new Node(data);
+    return node;
 }
  
 void PrintList (Node *head) {
-	if (head == NULL)
-		return;
- 
-	while (head != NULL) {
-		cout << head->data << "->";
-		head = head->next;
-	}
-	cout << "#" << endl;
+    if (head == NULL)
+        return;
+
+    while (head != NULL) {
+        cout << head->data << "->";
+        head = head->next;
+    }
+    cout << "#" << endl;
 }
 
 bool isLoopPresentUsingRabbitAndHare (Node *head) {
-	if (head == NULL)
-		return false;
+    if (head == NULL)
+        return false;
 
-	Node *fast = head;
-	Node *slow = head;
+    Node *fast = head;
+    Node *slow = head;
 
-	while (fast && fast->next) {
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-			return true;
-	}
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        // if at any point in time, these becomes
+        // equal, loop is present
+        if (slow == fast)
+            return true;
+    }
 
-	return false;
+    return false;
 }
 
 bool isLoopPresentUsingHashing (Node *head) {
-	if (head == NULL)
-		return false;
+    if (head == NULL)
+        return false;
 
-	unordered_set<Node*> visited;
+    unordered_set<Node*> visited;
 
-	Node *curr = head;
-	while (curr) {
-		if (visited.find(curr) != visited.end())
-			return true;
-		visited.insert(curr);
-		curr = curr->next;
-	}
+    Node *curr = head;
+    while (curr) {
+        if (visited.find(curr) != visited.end())
+            return true;
+        visited.insert(curr);
+        curr = curr->next;
+    }
 
-	return false;
+    return false;
 }
 
 int main() {
-	Node *head = NULL;
-	head = CreateNode(1);
-	head->next = CreateNode(2);
-	head->next->next = CreateNode(3);
-	head->next->next->next = CreateNode(4);
-	head->next->next->next->next = CreateNode(5);
+    Node *head = NULL;
+    head = CreateNode(1);
+    head->next = CreateNode(2);
+    head->next->next = CreateNode(3);
+    head->next->next->next = CreateNode(4);
+    head->next->next->next->next = CreateNode(5);
 
-	head->next->next->next->next->next = head->next->next;
+    head->next->next->next->next->next = head->next->next;
 
-	//PrintList (head);
+    //PrintList (head);
 
-	//isLoopPresentUsingRabbitAndHare(head) ? cout << "Loop is present!" : cout << "Loop is not present!";
-	isLoopPresentUsingHashing(head) ? cout << "Loop is present!" : cout << "Loop is not present!";
+    //isLoopPresentUsingRabbitAndHare(head) ? cout << "Loop is present!" : cout << "Loop is not present!";
+    isLoopPresentUsingHashing(head) ? cout << "Loop is present!" : cout << "Loop is not present!";
 
-	return 0;
+    return 0;
 }
