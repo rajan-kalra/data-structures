eb46846d1ce2e8cc2a88f1bcaa77990693056ef1
Rajan Kalra <rajan.kalra@hotmail.co.in>
2023-11-20 23:15:13 +0530

    Added removal of loop

--- linked-list/lenght-of-loop.cpp
+++ linked-list/lenght-of-loop.cpp
@@ -3,8 +3,8 @@
 using namespace std;
  
 class Node {
-	public:
-	    int data;
+    public:
+        int data;
         Node *next;
         Node (int data) {
             this->data = data;
@@ -13,81 +13,81 @@ class Node {
 };
  
 Node* CreateNode (int data) {
-	Node *node = new Node(data);
-	return node;
+    Node *node = new Node(data);
+    return node;
 }
  
 void PrintList (Node *head) {
-	if (head == NULL)
-		return;
- 
-	while (head != NULL) {
-		cout << head->data << "->";
-		head = head->next;
-	}
-	cout << "#" << endl;
+    if (head == NULL)
+        return;
+
+    while (head != NULL) {
+        cout << head->data << "->";
+        head = head->next;
+    }
+    cout << "#" << endl;
 }
 
 bool isLoopPresentUsingRabbitAndHare (Node *head) {
-	if (head == NULL)
-		return false;
+    if (head == NULL)
+        return false;
 
-	Node *fast = head;
-	Node *slow = head;
+    Node *fast = head;
+    Node *slow = head;
 
-	while (fast && fast->next) {
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-			return true;
-	}
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            return true;
+    }
 
-	return false;
+    return false;
 }
 
 int getLoopLength (Node *head) {
-	if (head == NULL || head->next == NULL)
-		return 0;
-	
-	Node *fast = head;
-	Node *slow = head;
-	Node *loopNode = NULL;
-	int count = 0;
+    if (head == NULL || head->next == NULL)
+        return 0;
 
-	while (fast && fast->next) {
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast) {
-			loopNode = slow;
-			break;
-		}
-	}
+    Node *fast = head;
+    Node *slow = head;
+    Node *loopNode = NULL;
+    int count = 0;
 
-	if (loopNode != NULL) {
-		count = 1;
-		Node *curr = loopNode->next;
-		while (curr != loopNode) {
-			count++;
-			curr = curr->next;
-		}
-	}
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            loopNode = slow;
+            break;
+        }
+    }
 
-	return count;
+    if (loopNode != NULL) {
+        count = 1;
+        Node *curr = loopNode->next;
+        while (curr != loopNode) {
+            count++;
+            curr = curr->next;
+        }
+    }
+
+    return count;
 }
 
 
 int main() {
-	Node *head = NULL;
-	head = CreateNode(1);
-	head->next = CreateNode(2);
-	head->next->next = CreateNode(3);
-	head->next->next->next = CreateNode(4);
-	head->next->next->next->next = CreateNode(5);
+    Node *head = NULL;
+    head = CreateNode(1);
+    head->next = CreateNode(2);
+    head->next->next = CreateNode(3);
+    head->next->next->next = CreateNode(4);
+    head->next->next->next->next = CreateNode(5);
 
-	head->next->next->next->next->next = head->next->next;
+    head->next->next->next->next->next = head->next->next;
 
-	//PrintList (head);
-	cout << "Length of loop: " << getLoopLength(head);
+    //PrintList (head);
+    cout << "Length of loop: " << getLoopLength(head);
 
-	return 0;
+    return 0;
 }
---
+++ linked-list/remove-loop.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+
+        Node (int d) {
+            data = d;
+            next = NULL;
+        }
+};
+
+int detectAndRemoveLoop(Node* head) {
+    Node *slow_p = head;
+    Node *fast_p = head;
+
+    // STEP 1: Iterate and find if loop exists or not
+    while (fast_p && fast_p->next) {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+
+        /* If slow_p and fast_p meet at some point then there is a loop */
+        if (slow_p == fast_p) {
+            removeLoop(slow_p, head);
+
+            /* Return 1 to indicate that loop was found & removed */
+            return 1;
+        }
+    }
+
+    /* Return 0 to indicate that no loop was found */
+    return 0;
+}
+
+void removeLoop(Node* loop_node, Node* head) {
+    Node* ptr1 = loop_node;
+    Node* ptr2 = loop_node;
+
+    // STEP 2: Count the number of nodes in loop
+    int k = 1;
+    while (ptr1->next != ptr2) {
+        ptr1 = ptr1->next;
+        k++;
+    }
+
+    // STEP 3.a: Fix one pointer to head
+    ptr1 = head;
+    // STEP 3.b: And the other pointer to k nodes after head
+    ptr2 = head;
+    for (int i = 0; i < k; i++)
+        ptr2 = ptr2->next;
+
+    /* STEP 4: Move both the pointers at the same pace, they will meet at loop starting node */
+    while (ptr2 != ptr1) {
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+
+    // Get pointer to the last node
+    while (ptr2->next != ptr1)
+        ptr2 = ptr2->next;
+
+    /* STEP 5: Set the next node of the loop ending node to fix the loop */
+    ptr2->next = NULL;
+}
+
+void printList(struct Node* node) {
+    // Print the list after loop removal
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+}
+
+struct Node* newNode(int key) {
+    Node *temp = new Node(key);
+    return temp;
+}
+
+int main() {
+    Node* head = newNode(50);
+    head->next = newNode(20);
+    head->next->next = newNode(15);
+    head->next->next->next = newNode(4);
+    head->next->next->next->next = newNode(10);
+
+    /* Create a loop for testing */
+    head->next->next->next->next->next = head->next->next;
+
+    detectAndRemoveLoop(head);
+
+    cout << "Linked List after removing loop \n";
+    printList(head);
+    return 0;
+}
